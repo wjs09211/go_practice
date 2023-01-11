04b387d4bafe32a29d23b1118fa7f09029b96874
kenny <kenny.wu@amazingtalker.com>
2023-01-11 20:04:06 +0800

    local cache unittest practice

---
+++ localcache/localcache.go
@@ -0,0 +1,48 @@
+// Package localcache
+package localcache
+
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+// Cache interface implement by localCache
+type Cache interface {
+	// Get value from key
+	Get(key string) (any, error)
+	// Set value by key
+	Set(key string, value any, expiredTime time.Duration) error
+}
+
+// New localCache
+func New() Cache {
+	return &localCache{make(map[string]*cacheData), sync.Mutex{}}
+}
+
+type localCache struct {
+	hash  map[string]*cacheData
+	mutex sync.Mutex
+}
+
+type cacheData struct {
+	data      any
+	expiredAt time.Time
+}
+
+func (obj *localCache) Get(key string) (any, error) {
+	if val, ok := obj.hash[key]; ok {
+		if val.expiredAt.Before(time.Now()) {
+			return nil, nil
+		}
+		return val.data, nil
+	}
+	return nil, errors.New("key not exist")
+}
+
+func (obj *localCache) Set(key string, value any, expiredTime time.Duration) error {
+	obj.mutex.Lock()
+	obj.hash[key] = &cacheData{value, time.Now().Add(expiredTime)}
+	defer obj.mutex.Unlock()
+	return nil
+}
---
+++ localcache/localcache_test.go
@@ -0,0 +1,66 @@
+package localcache
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type CacheSuite struct {
+	suite.Suite
+	cache *localCache
+}
+
+func (s *CacheSuite) SetupSuite() {
+	// call first, only once
+}
+
+func (s *CacheSuite) SetupTest() {
+	// call this before each test case
+	fmt.Println("SetupTest")
+	s.cache = New().(*localCache)
+}
+
+func (s *CacheSuite) TestSet() {
+	testKey := "key"
+	testValue := "value"
+	desc := "normal set"
+	err := s.cache.Set(testKey, testValue, time.Minute)
+	s.Require().Equal(err, nil, desc)
+	s.Require().Equal(s.cache.hash[testKey].data, testValue, desc)
+
+}
+
+func (s *CacheSuite) TestGet() {
+	testKey := "key"
+	testValue := "value"
+	desc := "normal get"
+	s.cache.hash[testKey] = &cacheData{testValue, time.Now().Add(time.Minute)}
+	val, err := s.cache.Get(testKey)
+	s.Require().Equal(err, nil, desc)
+	s.Require().Equal(val, testValue, desc)
+}
+
+func (s *CacheSuite) TestGetNoExistKey() {
+	testKey := "key"
+	desc := "no exist key"
+	val, err := s.cache.Get(testKey)
+	s.Require().NotEqual(err, nil, desc)
+	s.Require().Equal(val, nil, desc)
+}
+
+func (s *CacheSuite) TestCacheExpired() {
+	testKey := "key"
+	testValue := "value"
+	desc := "cache expired should return nil"
+	_ = s.cache.Set(testKey, testValue, time.Second)
+	time.Sleep(time.Second * 2)
+	val, err := s.cache.Get(testKey)
+	s.Require().Equal(err, nil, desc)
+	s.Require().Equal(val, nil, desc)
+}
+
+func TestStart(t *testing.T) {
+	suite.Run(t, new(CacheSuite))
+}
